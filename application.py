1174e6e9fb76d2a4059dda01d42d7cc2c9a07439
Himanshu Wadhwa <107308345+Himanshuwadhwa1@users.noreply.github.com>
2023-07-14 01:23:18 +0530

    Update application.py add password

--- application.py
+++ application.py
@@ -70,7 +70,7 @@ def index():
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
                 reviews.append(mydict)
-            client = pymongo.MongoClient("mongodb+srv://usernamehimanshu999:<password>@cluster0.xkm0aj4.mongodb.net/?retryWrites=true&w=majority")
+            client = pymongo.MongoClient("mongodb+srv://usernamehimanshu999:pwskills@cluster0.xkm0aj4.mongodb.net/?retryWrites=true&w=majority")
             db = client['review_scrap']
             review_col = db['review_scrap_data']
             review_col.insert_many(reviews)
